215a85d65ac613c9af727170e90ff44cce52d2cc
Nikhil <jkdman63@gmail.com>
2016-07-02 10:27:23 -0400

    main route

---
+++ src/views/Main/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container =  React.createClass({
+  render() {
+    return (
+      <div>
+        Hello from the container
+      </div>
+    )
+  }
+})
+
+export default Container
