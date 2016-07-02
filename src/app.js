59ee1b2e23ceb429abb549cde321688197304c8c
Nikhil <jkdman63@gmail.com>
2016-07-02 10:20:39 -0400

    move routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import 'font-awesome/css/font-awesome.css'
 import './app.css'
-
 import {browserHistory, Router, Route} from 'react-router'
-
-const Home = React.createClass({
-  render: function() {
-    return (<div>Hello world</div>)
-  }
-})
-
-const routes = (
-  <Router>
-    <Route path="/" component={Home} />
-  </Router>
-)
-
 import App from './containers/App/App'
+import makeRoutes from './routes'
+
+const routes = makeRoutes()
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(
